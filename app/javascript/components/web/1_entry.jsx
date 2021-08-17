b30461ce3b038160a60a648a37c48374b05e7c52
Sam Taylor <sam.taylor@mjh.co.nz>
2021-08-17 12:03:27 +1200

    initial commit

---
+++ app/javascript/components/web/1_entry.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Entry() {
+  return <div>Hi</div>;
+}
---
+++ app/javascript/packs/web.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Entry from "components/web/1_entry";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Entry />, document.getElementById("react_container"));
+});
