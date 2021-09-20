fc329c762f15c27852dc4114f7638a26ae6e91ad
Sam Taylor <sam.taylor@mjh.co.nz>
2021-09-21 11:33:16 +1200

    add test for deleting an employee

--- app/javascript/components/web/tests/e2e.test.js
+++ app/javascript/components/web/tests/e2e.test.js
@@ -107,5 +107,49 @@ describe("Employees test", () => {
     expect(lastNameColumn).toEqual("test 1");
   });
 
+  it("Create a new employee then deletes them", async () => {
+    // Load home page and get the initial length of employees table
+    await page.goto("http://localhost:3000/?tab=1");
+    await page.waitForResponse("http://localhost:3000/employees.json");
+    await page.waitForSelector("table");
+    const tbodyLength = await page.$eval("table tbody", (t) => t.rows.length);
+    // Go to new employee form and fill out then submit
+    await expect(page).toClick("button", { text: "New Employee +" });
+    await page.waitForSelector("form");
+    await expect(page.url()).toEqual("http://localhost:3000/employees/new");
+    await expect(page).toFillForm("form", {
+      fname: "new employee",
+      lname: "test",
+    });
+    await expect(page).toClick("button", { text: "Submit" });
+    await page.waitForResponse("http://localhost:3000/employees.json");
+    // Wait for table to render then compare length to previous
+    await page.waitForSelector("table");
+    await expect(page.url()).toEqual("http://localhost:3000/?tab=1");
+    const newTbodyLength = await page.$eval(
+      "table tbody",
+      (t) => t.rows.length
+    );
+    expect(newTbodyLength).toEqual(tbodyLength + 1);
+    // Find and click delete button for the new employee
+    const deleteId = await page.$eval(
+      `table > tbody > tr:nth-child(${newTbodyLength}) > td:nth-child(2)`,
+      (t) => t.textContent
+    );
+    await expect(page).toClick(
+      `table > tbody > tr:nth-child(${newTbodyLength}) > td:nth-child(5) > button`,
+      { text: "Delete" }
+    );
+    await page.waitForResponse(
+      `http://localhost:3000/employees/${deleteId}.json`
+    );
+    // Check that the employee has been deleted
+    const deletedTbodyLength = await page.$eval(
+      "table tbody",
+      (t) => t.rows.length
+    );
+    expect(deletedTbodyLength).toEqual(tbodyLength);
+  });
+
   // PROJECT TESTS
 });
